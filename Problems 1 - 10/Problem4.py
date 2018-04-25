795a948673aa5f555597a175a89d47d57281fccf
Daniel Ahn <xNinjaKittyx@users.noreply.github.com>
2018-04-25 12:59:23 -0700

    Update Problem4.py

--- Problems 1 - 10/Problem4.py
+++ Problems 1 - 10/Problem4.py
@@ -10,5 +10,8 @@
 #             break
 # print(highest)
 
-# One liner
-print(max([z for z in (x * y for x in range(999, 99, -1) for y in range(x, 99, -1)) if str(z) == str(z)[::-1]]))
+# One liner, Not necessarily the fastest it could be.
+print(max((z for z in (x * y for x in range(999, 99, -1) for y in range(x, 99, -1)) if str(z) == str(z)[::-1])))
+
+# Using map. About 40% speed increase on my Windows VM
+print(max(map(int, (z for z in map(str, (x * y for x in range(999, 99, -1) for y in range(x, 99, -1))) if z == z[::-1]))))
