9626a0093f1708f1727467234e67639e993e631f
Daniel Ahn <xNinjaKittyx@users.noreply.github.com>
2018-04-25 12:46:49 -0700

    Update Problem3.py

--- Problems 1 - 10/Problem3.py
+++ Problems 1 - 10/Problem3.py
@@ -1,29 +1,41 @@
 
 # Since we know that this is not divisible by 2, 3, 4, 5, 6, I will start the
 # algorithm at the 7th mark.
-list = []
+import random
+
+
+def composite_prime_check(n, k):
+    # Using Miller-Rabin
+    if n == 2:
+        return True
+    if not n % 2:
+        return False
+    
+    r, s = 0, n -1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+        
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
 number = 600851475143
-for x in range(71, 1234169, 2):
+for x in range(3, number // 2,  2):
     if number % x == 0:
-        list.append(x)
-        list.append(int(number/x))
-    x += 1
+        # If the number is divisible...
+        # Check if its a prime.
+        check_num = number // x
+        if composite_prime_check(check_num, 5):
+            print(check_num)
+            return
 
-list.sort()
-list.reverse()
-
-for x in list:
-    count = 2
-    prime = True
-    while count < x:
-        if x % count == 0:
-            prime = False
-            break
-        count += 1
-
-    if prime:
-        print(x)
-        break
-
-
-a = input()
