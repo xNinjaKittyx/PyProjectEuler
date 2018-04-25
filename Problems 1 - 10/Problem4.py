b00d024c00572a60ce87140aafa23510d88bbdf0
Daniel Ahn <xNinjaKittyx@users.noreply.github.com>
2018-04-25 12:56:03 -0700

    One Liner.

--- Problems 1 - 10/Problem4.py
+++ Problems 1 - 10/Problem4.py
@@ -1,18 +1,14 @@
 
+# highest = 0
+# for x in range(999, 99, -1):
+#     for y in range(x, 99, -1):
+#         number = x * y
+#         numberstring = str(number)
+#         if numberstring == numberstring[::-1]
+#             if number > highest:
+#                 highest = number
+#             break
+# print(highest)
 
-palindrome = True
-highest = 0
-for x in range(999, 99, -1):
-    for y in range(x, 99, -1):
-        number = x * y
-        numberstring = str(number)
-        for z in range(0, int(len(numberstring)/2)):
-            if not numberstring[z] == numberstring[-(z+1)]:
-                palindrome = False
-                break
-        if palindrome:
-            if number > highest:
-                highest = number
-            break
-        palindrome = True
-print(highest)
+# One liner
+print(max([z for z in (x * y for x in range(999, 99, -1) for y in range(x, 99, -1)) if str(z) == str(z)[::-1]]))
