ec02a95796b4c62ed05d9b04ee2bc7343e141b6b
Daniel Ahn <xNinjaKittyx@users.noreply.github.com>
2018-04-25 12:01:49 -0700

    Update Problem1.py
    One liner.

--- Problems 1 - 10/Problem1.py
+++ Problems 1 - 10/Problem1.py
@@ -1,14 +1,1 @@
-
-total = 0
-x = 3
-while x < 1000:
-    total += x
-    x += 3
-x = 5
-while x < 1000:
-    if x % 3 != 0:
-        total += x
-    x += 5
-
-print(total)
-x = input("Press something to exit")
+print(sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0]))
